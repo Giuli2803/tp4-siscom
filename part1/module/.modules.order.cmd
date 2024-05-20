ad11e822d0100e4ca2a847887a8940816718226f
giuliLinux <giulianopalombarini@gmail.com>
2024-05-20 14:33:11 -0300

    add make for module

---
+++ part1/module/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/oem/Desktop/TP4/tp4-siscom/part1/module/modules.order := {   echo /home/oem/Desktop/TP4/tp4-siscom/part1/module/mimodulo.ko; :; } | awk '!x[$$0]++' - > /home/oem/Desktop/TP4/tp4-siscom/part1/module/modules.order
